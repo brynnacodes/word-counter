e64999cda59e8f53972b153999a7d4a9da6fbaf4
Brynna <brynna.mccarter@gmail.com>
2017-02-17 08:40:15 -0800

    add third passing phpunit test for spec 3

--- tests/ReapeatCounterTest.php
+++ tests/ReapeatCounterTest.php
@@ -24,6 +24,30 @@ class RepeatCounterTest extends PHPUnit_Framework_TestCase
 
         $this->assertEquals(1, $result);
     }
+
+    //third test, spec 3: program counts a single occurence of a word in a multiple word string
+    function test_count_repeat_oneWordMultiString() {
+        $input1 = 'my name is john doe';
+        $input2 = 'name';
+        $test_repeat_counter = new RepeatCounter;
+
+        $result = $test_repeat_counter->CountRepeats($input1, $input2);
+
+        $this->assertEquals(1, $result);
+    }
+
+
+
+    //fourth test, spec 4: program counts a two occurences of a single word in a multiple word string
+    function test_count_repeat_multipleWordMultiString() {
+        $input1 = 'the cow jumped over the moon';
+        $input2 = 'the';
+        $test_repeat_counter = new RepeatCounter;
+
+        $result = $test_repeat_counter->CountRepeats($input1, $input2);
+
+        $this->assertEquals(2, $result);
+    }
 }
 
 ?>
