22b3fdf963a37565daca9d5746491259054f2628
Brynna <brynna.mccarter@gmail.com>
2017-02-17 13:01:12 -0800

    final debugging

--- src/RepeatCounter.php
+++ src/RepeatCounter.php
@@ -9,9 +9,10 @@
                     $stringInput = strtolower($input1);
                     $wordInput = strtolower($input2);
                     $result = 0;
-                    $wordArray = explode(" ", $input1);
+                    $wordArray = explode(" ", $stringInput);
+
                     foreach ($wordArray as $word) {
-                        if($word == $input2) {
+                        if($word == $wordInput) {
                             $result =  $result + 1;
                         }
                     }
@@ -20,24 +21,28 @@
                 else {
                     return false;
                 }
-            }
-            else {
+            } else {
                 return false;
             }
         }
 
 //this function gets and reads xml files and uses the merriam webster free api to look up word definitions for user input
-        function CheckWord($input2) {
+
+        function CheckWord($input2)
+        {
             $word_url = 'http://www.dictionaryapi.com/api/v1/references/collegiate/xml/'.$input2.'?key=59c48b7e-eff8-4bd0-9c5b-ad0781f94ca3';
             $response = file_get_contents($word_url);
+
             if ($response) {
                 $word_data = simplexml_load_string($response);
                 if ($word_data->entry->ew == $input2) {
                     return $word_data->entry->def->dt[0];
-                } else {
+                }
+                else {
                     return false;
                 }
-            } else {
+            }
+            else {
                 return false;
             }
         }
