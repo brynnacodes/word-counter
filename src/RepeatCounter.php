30fa79ea996e19026296fd1fc483c2cdad4679d1
Brynna <brynna.mccarter@gmail.com>
2017-02-17 08:36:16 -0800

    add RepeatCounter class, CountRepeats method, and first passing PHPunit test

--- src/RepeatCounter.php
+++ src/RepeatCounter.php
@@ -1,3 +1,15 @@
 <?php
+    class RepeatCounter
+    {
 
+        function CountRepeats($input1, $input2)
+        {
+          $stringInput = $input1;
+          $wordInput = $input2;
+
+          $result = substr_count($stringInput, $wordInput);
+
+          return $result;
+        }
+    }
 ?>
--- tests/ReapeatCounterTest.php
+++ tests/ReapeatCounterTest.php
@@ -1,3 +1,18 @@
 <?php
+require_once 'src/RepeatCounter.php';
+
+class RepeatCounterTest extends PHPUnit_Framework_TestCase
+{
+    //first test is for spec 1: program counts a single occurrence of a single letter in a string
+    function test_count_repeat_singleLetter() {
+        $input1 = 'a';
+        $input2 = 'a';
+        $test_repeat_counter = new RepeatCounter;
+
+        $result = $test_repeat_counter->CountRepeats($input1, $input2);
+
+        $this->assertEquals(1, $result);
+    }
+}
 
 ?>
