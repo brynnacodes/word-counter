2975a08653323f05f2b6fc05961938b3f99c0e99
Brynna <brynna.mccarter@gmail.com>
2017-02-17 09:11:48 -0800

    add twig files and minimal ui with form for user input

--- app/app.php
+++ app/app.php
@@ -1,14 +1,22 @@
 <?php
     date_default_timezone_set("America/Los_Angeles");
     require_once __DIR__."/../vendor/autoload.php";
-    require_once __DIR__."/../src/src.php";
+    require_once __DIR__."/../src/RepeatCounter.php";
 
     $app = new Silex\Application();
     $app->register(new Silex\Provider\TwigServiceProvider(), ["twig.path" => __DIR__."/../views"]);
 
     $app->get('/', function() use($app) {
-        $result = 'hello';
-        return $app["twig"]->render("root.html.twig", ['result' => $result]);
+
+        return $app["twig"]->render("root.html.twig");
+    });
+
+    $app->post('/see_count', function() use($app) {
+        $new_repeat_counter = new RepeatCounter;
+        $input1 = ($_POST['userinput1']);
+        $input2 = ($_POST['userinput2']);
+        $result = $new_repeat_counter->CountRepeats($input1, $input2);
+        return $app["twig"]->render("see_count.html.twig", ['result' => $result, 'input1' => $input1, 'input2' => $input2]);
     });
 
     return $app;
